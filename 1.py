86a8e152f18b2dd7f44536f9d107e458a7296345
Dmitry Budolak <maniacmasnion12@gmail.com>
2021-07-20 23:38:07 +0300

    Update 1.py
    автоматизация процесса

--- 1.py
+++ 1.py
@@ -1,17 +1,9 @@
 import numpy as np
+count = 0
+number = np.random.randint(1, 101)      # загадали число
 
-count = 0  # счетчик попыток
-number = np.random.randint(1, 101)  # загадали число
 print("Загадано число от 1 до 100")
-
-while True:  # бесконечный цикл
-    predict = int(input())  # предполагаемое число
-    count += 1  # плюсуем попытку
-    if number == predict:
-        break  # выход из цикла, если угадали
-    elif number > predict:
-        print(f"Угадываемое число больше {predict} ")
-    elif number < predict:
-        print(f"Угадываемое число меньше {predict} ")
-
+for count in range(1, 101):         # более компактный вариант счетчика
+    if number == count:
+        break      # выход из цикла, если угадали
 print(f"Вы угадали число {number} за {count} попыток.")
