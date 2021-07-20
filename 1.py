244d10c2fac7e54eb2bc746638be2dc1b8d738e0
Dmitry Budolak <maniacmasnion12@gmail.com>
2021-07-20 23:49:52 +0300

    Update 1.py
    Повторим 1000 раз, чтоб понять, насколько быстро в среднем работает наше решение. Для этого сразу завернём игру в функцию, чтобы её потом было проще менять и к ней обращаться.

--- 1.py
+++ 1.py
@@ -2,8 +2,28 @@ import numpy as np
 count = 0
 number = np.random.randint(1, 101)      # загадали число
 
-print("Загадано число от 1 до 100")
-for count in range(1, 101):         # более компактный вариант счетчика
-    if number == count:
-        break      # выход из цикла, если угадали
-print(f"Вы угадали число {number} за {count} попыток.")
+
+def game_core_v1(number):
+    # Просто угадываем на random, никак не используя информацию о больше или меньше.
+    # Функция принимает загаданное число и возвращает число попыток
+    count = 0
+    while True:
+        count += 1
+        predict = np.random.randint(1, 101)  # предполагаемое число
+        if number == predict:
+            return count  # выход из цикла, если угадали
+
+
+def score_game(game_core):
+    # Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число
+    count_ls = []
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1, 101, size=1000)
+    for number in random_array:
+        count_ls.append(game_core(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return(score)
+
+
+score_game(game_core_v1)   # в среднем за 101 попытку
